3a75b93b95101e24f5ff0b109a35311462590150
Iain Macaulay <iain_macaulay@hotmail.com>
2018-07-03 15:46:12 +0100

    added hash to pwd

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+ var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPwd ='$2a$10$DY1FUoEGxZsvvjDDAxkajeSseqjgGu6CCK7pLBj4zBbdCjprQBoLy';
+
+bcrypt.compare(password, hashedPwd).then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.log(err);
+});
 
 
 
-var token = jwt.sign(data, '123abc');
-console.log(`token: ${token}`);
 
-var decoded=jwt.verify(token, '123abc');
-
-console.log(decoded);
+// var token = jwt.sign(data, '123abc');
+// console.log(`token: ${token}`);
+//
+// var decoded=jwt.verify(token, '123abc');
+//
+// console.log(decoded);
 // var message = ' I am user no 3 ';
 // var hash=  SHA256(message).toString();
 //
